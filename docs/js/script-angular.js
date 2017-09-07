6f441096bf212ce897768a34b4a1d829b6722808
Animesh Jain <animesh.jain1203@gmail.com>
2017-09-07 15:47:34 +0530

    Major Bug Fixes in Calculator

--- docs/js/script-angular.js
+++ docs/js/script-angular.js
@@ -2,28 +2,20 @@
 var app = angular.module('app', ['ngRoute']);
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.when('/calculator', {
-        templateUrl: "calculator-new.html"
+        templateUrl: "docs/pages/calculator-new.html"
     }).when('/todo-list', {
-        templateUrl: "todo-list.html"
+        templateUrl: "docs/pages/todo-list.html"
     }).when('/notepad', {
-        templateUrl: "notepad.html"
+        templateUrl: "docs/pages/notepad.html"
     }).otherwise('/calculator', {
-        templateUrl: "calculator.html"
+        templateUrl: "docs/pages/calculator-new.html"
     });
     $locationProvider.html5Mode(true);
         }]);
 app.controller('MainController', function MainController($scope) {
-    $scope.num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    $scope.ops = ['+', '-', '/', '*', '='];
-    $scope.inputSeq = '';
-    $scope.first = '';
-    $scope.res = 0;
-    $scope.number = 0;
-    $scope.seq = [{
-        varNumber: "",
-        operator: ""
-    }];
-    $scope.navItems = [
+    $scope.NUM = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    $scope.OPS = ['+', '-', '/', '*', '='];
+    $scope.NAV_ITEMS = [
         {
             title: 'Calculator',
             ref: 'calculator'
@@ -35,6 +27,15 @@ app.controller('MainController', function MainController($scope) {
             ref: 'notepad'
                 }
             ];
+    $scope.inputSeq = '';
+    $scope.first = '';
+    $scope.res = 0;
+    $scope.number = 0;
+    $scope.seq = [{
+        varNumber: "",
+        operator: ""
+    }];
+
 
 
     $scope.set = function (item) {
@@ -43,14 +44,20 @@ app.controller('MainController', function MainController($scope) {
 
 
     $scope.setVar = function (index) {
-        $scope.first = $scope.first + $scope.num[index];
+        if ($scope.seq[0].varNumber && !$scope.seq[0].operator) {
+            $scope.seq[0].varNumber = '';
+            $scope.first = '';
+
+        }
+        $scope.first = $scope.first + $scope.NUM[index];
         $scope.inputSeq = $scope.first;
+
     };
 
-
+    // Creating operand if number is being entered or creating 'operator' if operator is entered EX: 123 + 1234 = 
     $scope.setOp = function (item) {
 
-        $scope.op = $scope.ops[item];
+        $scope.op = $scope.OPS[item];
 
         if ($scope.op === '=') {
             $scope.seq.push({
@@ -62,22 +69,27 @@ app.controller('MainController', function MainController($scope) {
         } else {
             //  console.log(' ' + $scope.op + ' ');
             // $scope.res = $scope.res + parseInt($scope.first);
+            var length = $scope.seq.length;
             if ($scope.seq[0].varNumber && !$scope.seq[0].operator) {
                 $scope.seq[0].operator = $scope.op;
             } else if (!$scope.seq[0].varNumber && !$scope.seq[0].operator) {
                 $scope.seq[0].varNumber = $scope.first;
                 $scope.seq[0].operator = $scope.op;
+            } else if ($scope.seq[length - 1].varNumber && !$scope.first) {
+                $scope.seq[length - 1].operator = $scope.op;
             } else {
                 $scope.seq.push({
                     varNumber: $scope.first,
                     operator: $scope.op
                 });
             }
-            // console.log($scope.seq);
+            console.log($scope.seq);
             $scope.first = '';
         }
     };
+    //((parseFloat(inputSeq) < 0 )?((parseFloat(inputSeq)*-1) + '-') :(inputSeq || 0 ))
 
+    // Using loop to add 'varNumber' in 'seq' variable
     $scope.calculate = function (item) {
 
         for (var i = 0; i < item.length - 1; i++) {
@@ -85,8 +97,8 @@ app.controller('MainController', function MainController($scope) {
             var operand_one = item[i].varNumber;
             var operand_two = item[i + 1].varNumber;
             var operator = item[i].operator;
-            $scope.inputSeq = $scope.calc(operand_one, operand_two, operator);
-            item[i + 1].varNumber = $scope.inputSeq;
+            $scope.inputSeq = $scope.calc(operand_one, operand_two, operator); // call calc function to do operations
+            item[i + 1].varNumber = $scope.inputSeq; // update next varNumber to the current result
             console.log($scope.calc(operand_one, operand_two, operator));
         }
         $scope.seq = [];
@@ -94,13 +106,14 @@ app.controller('MainController', function MainController($scope) {
             varNumber: $scope.inputSeq,
             operator: ''
         });
-        $scope.first = $scope.inputSeq;
+        $scope.first = $scope.inputSeq; // If someone chooses to continue operations after '=' operation
+
         console.log($scope.seq);
     };
 
-
-    $scope.calc = function (one, two, ope) {
-        switch (ope) {
+    //Operation based on value of operator otherwise return empty string
+    $scope.calc = function (one, two, operator) {
+        switch (operator) {
             case '+':
                 return parseFloat(one) + parseFloat(two);
             case '-':
@@ -114,6 +127,8 @@ app.controller('MainController', function MainController($scope) {
         }
 
     };
+
+    // Using 'C' to clear all variables
     $scope.clear = function () {
         $scope.seq = [{
             varNumber: "",
